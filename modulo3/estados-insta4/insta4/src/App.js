d6b51a5face8a95d8f73d316089205f727542577
erica-l-s <erica.l.s1996@gmail.com>
2022-04-26 21:56:12 -0300

    insta4

---
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post
+        nomeUsuario={'praia'}
+        fotoUsuario={'https://picsum.photos/50/49'}
+        fotoPost={'https://picsum.photos/200/149'}
+        />
+
+        <Post
+        nomeUsuario={'curtindo as ferias'}
+        fotoUsuario={'https://picsum.photos/50/48'}
+        fotoPost={'https://picsum.photos/200/148'}
+        />
+
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
