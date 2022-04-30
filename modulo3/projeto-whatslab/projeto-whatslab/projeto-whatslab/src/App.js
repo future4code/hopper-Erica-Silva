bf2bd50c8abe4f59557103c304d760a0fead7060
erica-l-s <erica.l.s1996@gmail.com>
2022-04-29 21:15:13 -0300

    irei fazer no fds

---
+++ modulo3/projeto-whatslab/projeto-whatslab/projeto-whatslab/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+     <input></input>
+     <input></input>
+     <button>Enviar</button>
+    </div>
+    </div>
+  );
+}
+
+export default App;
